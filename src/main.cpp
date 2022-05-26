4f63700d4e65021925c320539e0f2147bcb7ba9c
Tobias Stein <tobiasstein1@gmx.net>
2022-05-26 23:24:16 +0200

    Successfull WiFi Setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,84 @@
 #include <Arduino.h>
-#include "WiFi.h"
+#include <Preferences.h>
 #include "SPIFFS.h"
 #include "ESPAsyncWebServer.h"
-#include "ArduinoJson.h"
-#include "networks.hpp"
 
 const char* apSSID = "ESP-Epaper";
 const char* apPassword = "123456789";
+Preferences preferences;
 
 AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello");
+  preferences.begin("wifi");
+  if(!preferences.isKey("ssid")){ // initializes keys during first run
+    preferences.putString("ssid","");
+    preferences.putString("password","");
+  }
+
   if(!SPIFFS.begin()){
     Serial.println("An Error has occurred while mounting SPIFFS");
   }
 
-  JsonArrayConst arr = getNetworks();
-  for(JsonVariantConst v : arr){
-    Serial.println(v["SSID"].as<const char*>());
+  WiFi.disconnect();
+  String ssid = preferences.getString("ssid","");
+  String password = preferences.getString("password", "");
+  Serial.print("Stored SSID is: ");
+  Serial.println(ssid);
+
+  if(ssid!=""){ // try to connect to known Access Point
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(),password.c_str());
+    for(int i=0;i<10 || WiFi.status() != WL_CONNECTED;++i){
+      Serial.print('.');
+      delay(1000);
+    }
   }
-
-  WiFi.softAP(apSSID, apPassword);
-
-  server.on("/html", HTTP_GET, [](AsyncWebServerRequest *request){
+  if(!WiFi.isConnected()){ // Setup Hotspot if no WiFi connection is possible
+    Serial.println("Failed to connect to WiFi. Starting Access Point");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(apSSID, apPassword);
+  }
+  else{
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+  }
+  
+  server.on("/setup", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/setup.html", "text/html");
   });
 
+  server.on("/setup", HTTP_POST, [](AsyncWebServerRequest *request){
+    Serial.println("Received Post");
+    int params = request->params();
+      for(int i=0;i<params;i++){
+        AsyncWebParameter* p = request->getParam(i);
+        if(p->isPost()){
+          Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+        else{
+          Serial.printf("GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+      }
+      if(request->hasParam("ssid", true)){
+        AsyncWebParameter* p = request->getParam("ssid", true);
+        Serial.print("Saving SSID: ");
+        Serial.println(p->value());
+        preferences.putString("ssid",p->value());
+      }
+      if(request->hasParam("password",true)){
+        AsyncWebParameter* p = request->getParam("password", true);
+        Serial.print("Saving password: ");
+        Serial.println(p->value());
+        preferences.putString("password",p->value());
+      }
+      if(request->hasParam("clearsettings",true)){
+        Serial.println("Clearing settings");
+        preferences.clear();
+      }
+      ESP.restart();
+  });
   server.begin();
 }
 
--- src/networks.hpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include "ArduinoJson.h"
-#include "SPIFFS.h"
-
-std::string readfile(const char* filename);
-JsonArrayConst getNetworks();
-void addNetwork(char* SSID, char* PW);
